3e5caa41d099577f75b713fdc6a5373a181f24f7
Force-in-Motion <canzdao@gmail.com>
2024-10-13 22:01:09 +0300

    feat config_history : add data config for objects module history

---
+++ src/history/config_purchase_history.py
@@ -0,0 +1,87 @@
+'-------ScrollPurchaseHistory-------'
+ft_sl = ('Helvetica', 18, 'bold')
+hc_sl = '#453E3E'
+fgc_sl = '#434141'
+bw_sl = 1
+
+'-------ButtonsMenuPurchaseHistory-------'
+
+'config label open list'
+tc_all = 'white'
+tt_all = 'Открыть список'
+ft_all = ('Helvetica', 18, 'bold')
+
+'config label restore list'
+tc_rl = 'white'
+tt_rl = 'Восстановить список'
+ft_rl = ('Helvetica', 18, 'bold')
+
+
+'config menu buttons'
+size_ol = (40, 40)
+
+wh_ol = 0
+ht_ol = 0
+tt_ol = ''
+fgc_ol = 'transparent'
+hc_ol = '#262424'
+
+tt_dp = "Удалить выделенное"
+wh_dp = 220
+fgc_dp = '#CD5C5C'
+ht_dp = 50
+tc_dp = 'white'
+bw_dp = 1
+hc_dp = '#783D3D'
+ft_dp = ('Helvetica', 15, 'bold')
+
+tt_ch = "Очистить историю"
+wh_ch = 220
+ht_ch = 50
+fgc_ch = '#CD5C5C'
+tc_ch = 'white'
+bw_ch = 1
+hc_ch = '#783D3D'
+ft_ch = ('Helvetica', 15, 'bold')
+
+tt_cb = "Назад"
+wh_cb = 220
+fgc_cb = '#CD5C5C'
+ht_cb = 50
+tc_cb = 'white'
+bw_cb = 1
+hc_cb = '#783D3D'
+ft_cb = ('Helvetica', 15, 'bold')
+
+
+
+'-------PurchaseHistory-------'
+
+'config window'
+ttl_cw = 'История покупок'
+gt_cw = '900x700+600+100'
+rzb_wh = False
+rzb_ht = False
+
+'config logo'
+size_l = (240, 200)
+tt_l = ''
+
+'config scroll frame'
+wh_sf = 520
+ht_sf = 400
+fgc_sf = 'transparent'
+cr_sf = 0
+
+'config menu buttons'
+wh_mb = 900
+ht_mb = 250
+fgc_mb = 'transparent'
+cr_mb = 0
+
+path_logo = r'Images\logo.png'
+path_round_button = r'Images\round_button.png'
+path_reset_button = r'Images\reset.png'
+
+
+
