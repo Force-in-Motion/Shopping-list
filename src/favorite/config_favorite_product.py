f19eee73b644483019114d9dfd4b564adc0c2627
Force-in-Motion <canzdao@gmail.com>
2024-10-13 21:49:40 +0300

    feat config_favorite : add data config in file for objects module favorite

---
+++ src/favorite/config_favorite_product.py
@@ -0,0 +1,81 @@
+'-------ButtonMenuFavoriteProducts-------'
+
+'config label add product'
+tc_all = 'white'
+tt_all = 'Добавить товар'
+ft_all = ('Helvetica', 18, 'bold')
+
+
+'config buttons menu'
+size_ali = (40, 40)
+
+wh_alb = 0
+ht_alb = 0
+tt_alb = ''
+fgc_alb = 'transparent'
+hc_alb = '#262424'
+
+tt_db = "Удалить выделенное"
+wh_db = 180
+ht_db = 50
+fgc_db = '#CD5C5C'
+tc_db = 'white'
+bw_db = 1
+hc_db = '#783D3D'
+ft_db = ('Helvetica', 15, 'bold')
+
+
+tt_cbt = "Очистить историю"
+wh_cb = 180
+fgc_cb = '#CD5C5C'
+ht_cb = 50
+tc_cb = 'white'
+bw_cb = 1
+hc_cb = '#783D3D'
+ft_cb = ('Helvetica', 15, 'bold')
+
+
+tt_cab = "Назад"
+wh_cab = 220
+ht_cab = 50
+fgc_cab = '#CD5C5C'
+tc_cab = 'white'
+bw_cab = 1
+hc_cab = '#783D3D'
+ft_cab = ('Helvetica', 15, 'bold')
+
+
+'-------FavoriteProducts-------'
+
+'config window'
+ttl = 'Ибранное'
+gt = '900x700+600+100'
+
+
+'config logo'
+size_l = (240, 200)
+tt_l = ''
+
+
+'config scroll place frame'
+wh_sp = 520
+ht_sp = 400
+fgc_sp = 'transparent'
+cr_sp = 0
+
+
+'config button frame'
+wh_bm = 900
+ht_bm = 250
+fgc_bm = 'transparent'
+cr_bm = 0
+
+'config checkbox'
+
+ft_sl = ('Helvetica', 18, 'bold')
+hc_sl = '#453E3E'
+fgc_sl = '#434141'
+bw_sl = 1
+
+path_logo = r'Images\logo.png'
+path_round_button = r'Images\round_button.png'
