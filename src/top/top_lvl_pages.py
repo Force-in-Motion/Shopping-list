9653798fa2cb56a5bc25aa9387d7e37c42cbd9e8
Force-in-Motion <canzdao@gmail.com>
2024-10-14 13:21:13 +0300

    feat Templates : add method init  for class AddNewCategory module top_lvl_pages

--- src/top/top_lvl_pages.py
+++ src/top/top_lvl_pages.py
@@ -5,3 +5,20 @@ from tkinter.messagebox import showerror
 from src.top.config_top_level_pages import *
 from src.load.save_and_load_data import SaveAndLoadData as sld
 from src.templates.templates import Templates
+
+class AddNewCategory(ctk.CTkToplevel):
+    """
+    Класс, описывающий функционал окна верхнего уровня и его виджеты
+    """
+    def __init__(self, main_window, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.__main_window = main_window
+        self.__list_categories = sld.read_categories()
+
+        self.__input_field = None
+
+        self.__config_logo()
+        self.__config_window()
+        self.__config_input_field()
+        self.__config_menu_buttons()
