415a18200844f77b8afe250cee827d742ead8b01
Force-in-Motion <canzdao@gmail.com>
2024-10-14 12:06:31 +0300

    feat open_list : add class OpenList  in open_list module

--- src/open/open_list.py
+++ src/open/open_list.py
@@ -41,3 +41,37 @@ class MenuButtonsOpenList(ctk.CTkFrame):
         self.__config_menu_buttons()
         self.__config_label_add_product_in_list()
         self.__config_label_add_product_in_favorite()
+
+
+
+class OpenList(ctk.CTkToplevel):
+    """
+    Мэйн класс страницы, в себе формирует основные контейнеры (фреймы), содержащие остальные виджеты страницы
+    """
+
+    def __init__(self, main_window: AllLists, scroll_all_lists: ScrollAllLists):
+        super().__init__()
+        self.__main_window = main_window
+
+        self.__load_data = sld.read_data_with_shopping_lists() if sld.check_file_shopping_lists() else {}
+        self.__load_data_favorites = sld.read_data_with_favorites_products() if sld.check_file_favorites_products() else {
+            "f": []}
+        self.__list_categories = sld.read_categories()
+
+        self.__scroll_all_lists = scroll_all_lists
+
+        self.__list_products = []
+
+        self.__category_product = None
+        self.__scroll_open_list = None
+        self.__menu_btn_open_list = None
+
+        self.__add_product_page = None
+        self.__edit_product_page = None
+        self.__confirmation_request_page = None
+
+        self.__config_window()
+        self.__config_logo()
+        self.__config_menu_buttons()
+        self.__config_scroll_frame()
+        self.__config_menu_sorted()
