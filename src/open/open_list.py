8507bfd6793bb3994997b95eb1de8fc841a05998
Force-in-Motion <canzdao@gmail.com>
2024-10-14 12:03:58 +0300

    feat open_list : add class ScrollOpenListProducts in open_list module

--- src/open/open_list.py
+++ src/open/open_list.py
@@ -10,3 +10,18 @@ from src.load.save_and_load_data import SaveAndLoadData as sld
 from src.open.config_open_list import *
 from src.top.top_lvl_pages import AddProduct, EditProduct, ConfirmationPage
 
+
+
+class ScrollOpenListProducts(ctk.CTkScrollableFrame):
+    """
+    Класс- контейнер, формирует область со скролом для работы с добавленными товарами либо для добавления новых
+    """
+
+    def __init__(self, main_window: OpenList, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__main_window = main_window
+        self.__list_checkboxes = []
+
+        self.__list_name = None
+
+        self.__load_checkbox_products()
