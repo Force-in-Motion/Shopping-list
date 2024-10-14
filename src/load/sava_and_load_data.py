108c0c638e45be255b4e733cc0578688b5dc5d03
Force-in-Motion <canzdao@gmail.com>
2024-10-14 10:49:58 +0300

    feat SaveAndLoadData : add static class method check_file_shopping_lists

--- src/load/sava_and_load_data.py
+++ src/load/sava_and_load_data.py
@@ -11,3 +11,16 @@ class SaveAndLoadData:
         """
         if not os.path.isdir(path_dir):
             os.mkdir(path_dir)
+
+
+    @staticmethod
+    def check_file_shopping_lists() -> bool:
+        """
+        Проверяет наличие файла txt по указанному пути, в данном случае в папке
+        :return: True или False
+        """
+        file = path_dir + r'\shopping_lists.json'
+        if os.path.isfile(file):
+            return True
+        else:
+            return False
