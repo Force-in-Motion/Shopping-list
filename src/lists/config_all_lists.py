e384362b8bddfb330d235b1c35069f039584fd3c
Force-in-Motion <canzdao@gmail.com>
2024-10-14 10:34:13 +0300

    feat config_all_lists : add config data for objects all_lists module

---
+++ src/lists/config_all_lists.py
@@ -0,0 +1,66 @@
+'-------ButtonsMenuAddList-------'
+
+'config menu buttons'
+wh_m = 220
+ht_m = 50
+fgc_m = '#CD5C5C'
+hc_m = '#783D3D'
+ft_m = ('Helvetica', 15, 'bold')
+tc_m = 'white'
+bw_m = 1
+
+ol_tt = "Открыть"
+el_tt = "Редактировать"
+dl_tt = "Удалить"
+cl_tt = "Назад"
+
+'config add list button'
+size_alb = (40, 40)
+fgc_alb = 'transparent'
+hc_alb = '#262424'
+wh_alb = 0
+ht_alb = 0
+tt_alb = ''
+
+'config label add list'
+tc = 'white'
+tt_lal = 'Добавить список покупок'
+ft_lal = ('Helvetica', 18, 'bold')
+
+
+'-------AllLists-------'
+
+'config window'
+ttl = 'Все списки покупок'
+gt = '900x700+600+100'
+rsb_wh = False
+rsb_ht = False
+
+
+'config scroll frame'
+wh_sf = 520
+ht_sf = 350
+cr_sf = 1
+fgc_sf = 'transparent'
+
+
+'config menu buttons'
+wh_bf = 900
+ht_bf = 280
+fgc_bf = 'transparent'
+
+
+'config logo'
+size_l = (240, 200)
+tt_l = ''
+
+
+'-------ScrollAllList-------'
+ft_sl = ('Helvetica', 18, 'bold')
+hc_sl = '#453E3E'
+fgc_sl = '#434141'
+bw_sl = 1
+
+
+path_logo = r'Images\logo.png'
+path_round_button = r'Images\round_button.png'
