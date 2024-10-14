51a737f3ef16d2aa3dc415c59ee5c27e11492d13
Force-in-Motion <canzdao@gmail.com>
2024-10-14 12:12:16 +0300

    feat main_page : add class MainFrame  with min_page

--- src/start/main_page.py
+++ src/start/main_page.py
@@ -8,3 +8,16 @@ from src.create.create_list import *
 from src.favorite.favorite_product import *
 from src.history.purchase_history import *
 from src.open.open_list import *
+
+
+class MainFrame(ctk.CTkFrame):
+    """
+    Класс, реализующий главное окно приложения
+    """
+    def __init__(self, main_window, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__main_window = main_window
+
+        self.__config_logo()
+        self.__config_menu_buttons()
+        self.__config_exit_button()
