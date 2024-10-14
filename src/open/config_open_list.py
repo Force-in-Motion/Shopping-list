c75e7b3367f29bdcb2c120e4b6374328e49b69e5
Force-in-Motion <canzdao@gmail.com>
2024-10-14 14:04:40 +0300

    feat open_list : add config file from open_list module

---
+++ src/open/config_open_list.py
@@ -0,0 +1,120 @@
+'-------ScrollOpenListProducts-------'
+
+'config favorite button'
+size_if = (40, 40)
+
+wh_bf = 0
+ht_bf = 0
+tt_bf = ''
+fgc_bf = 'transparent'
+hc_bf = '#262424'
+
+'config checkbox'
+
+ft_sl = ('Helvetica', 18, 'bold')
+hc_sl = '#453E3E'
+fgc_sl = '#434141'
+bw_sl = 1
+
+'-------ButtonsMenuOpenListProducts-------'
+
+'config label add product in list'
+tc_apl = 'white'
+tt_apl = 'Добавить товар'
+ft_apl = ('Helvetica', 18, 'bold')
+
+'config label add product in favorite'
+tc_f = 'white'
+tt_f = 'Добавить в избранное'
+ft_f = ('Helvetica', 18, 'bold')
+
+'config menu buttons'
+size_api = (40, 40)
+wh_ap = 0
+ht_ap = 0
+tt_ap = ''
+fgc_ap = 'transparent'
+hc_ap = '#262424'
+
+tt_ep = "Редактировать"
+wh_ep = 220
+ht_ep = 50
+fgc_ep = '#CD5C5C'
+tc_ep = 'white'
+bw_ep = 1
+hc_ep = '#783D3D'
+ft_ep = ('Helvetica', 15, 'bold')
+
+tt_dp = "Удалить"
+wh_dp = 220
+ht_dp = 50
+fgc_dp = '#CD5C5C'
+tc_dp = 'white'
+bw_dp = 1
+hc_dp = '#783D3D'
+ft_dp = ('Helvetica', 15, 'bold')
+
+tt_cb = "Назад"
+wh_cb = 220
+ht_cb = 50
+fgc_cb = '#CD5C5C'
+tc_cb = 'white'
+bw_cb = 1
+hc_cb = '#783D3D'
+ft_cb = ('Helvetica', 15, 'bold')
+
+
+
+'--------ListProducts-------'
+
+'config window'
+ttl_cw = 'Список товаров'
+gt_cw = '900x700+600+100'
+rzb_wh = False
+rzb_ht = False
+
+
+'config logo'
+size_l = (240, 200)
+tt_l = ''
+
+
+'config scroll frame'
+wh_sf = 520
+ht_sf = 350
+fgc_sf = 'transparent'
+cr_sf = 0
+
+
+'config menu buttons'
+wh_mb = 900
+ht_mb = 280
+fgc_mb = 'transparent'
+cr_mb = 0
+
+'config label sort menu'
+tc_slb = 'white'
+tt_slb = 'Сортировать по категории:'
+ft_slb = ('Helvetica', 17, 'bold')
+
+'config_sorted_meny'
+size_sp = (40, 40)
+wh_sp = 0
+ht_sp = 0
+tt_sp = ''
+fgc_sp = 'transparent'
+hc_sp = '#262424'
+
+'config_category_place'
+tc_c = '#706C6C'
+wh_c = 180
+ht_c = 40
+fgc_c = 'white'
+ft_c = ('Helvetica', 15, 'bold')
+st_c = 'readonly'
+bc_c = '#3D3935'
+
+path_logo = r'Images\logo.png'
+path_round_button = r'Images\round_button.png'
+path_favorite_button = r'Images\heart.png'
+path_glass_button = r'Images\glass.png'
