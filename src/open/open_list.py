84a6f3b03de9bb05ef77e405bfa02226dcf5af56
Force-in-Motion <canzdao@gmail.com>
2024-10-14 12:02:10 +0300

    feat open_list : add imports

---
+++ src/open/open_list.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from PIL import Image
+import customtkinter as ctk
+from tkinter.messagebox import showerror, showinfo
+
+
+from src.lists.all_lists import *
+from src.load.save_and_load_data import SaveAndLoadData as sld
+from src.open.config_open_list import *
+from src.top.top_lvl_pages import AddProduct, EditProduct, ConfirmationPage
+
