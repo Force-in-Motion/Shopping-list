9aed4c6275f56ab3eff8646a4715cf72b9e35996
Force-in-Motion <canzdao@gmail.com>
2024-10-14 10:48:55 +0300

    feat SaveAndLoadData : add static class SaveAndLoadData and method create_folder

--- src/load/sava_and_load_data.py
+++ src/load/sava_and_load_data.py
@@ -1,3 +1,13 @@
 import json
 import os
 import os.path
+
+class SaveAndLoadData:
+
+    @staticmethod
+    def create_folder() -> None:
+        """
+        Проверяет наличие папки по указанному адресу, если папка отсутствует то создает ее
+        """
+        if not os.path.isdir(path_dir):
+            os.mkdir(path_dir)
