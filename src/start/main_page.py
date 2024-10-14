b073aba88c6c86f24ae705ae2831ecac22d08727
Force-in-Motion <canzdao@gmail.com>
2024-10-14 12:18:23 +0300

    feat main_page : add method __config_menu_buttons for class MainPage with main_page module

--- src/start/main_page.py
+++ src/start/main_page.py
@@ -22,6 +22,41 @@ class MainFrame(ctk.CTkFrame):
         self.__config_menu_buttons()
         self.__config_exit_button()
 
+    def __config_menu_buttons(self) -> None:
+        """
+        Формирует в себе кнопки, отвечающие за общий функционал приложения, а так же устанавливает его параметры и стили
+        """
+        self.__add_list = ctk.CTkButton(self, text=al_tt, width=wh_al, fg_color=fgc_mf, border_color=bc_mf,
+                                      height=ht_mf, text_color=tc_mf, border_width=bw_m, hover_color=hc_mf, font=ft_mf)
+        self.__add_list.place(relx=0.05, rely=0.2)
+        self.__add_list.configure(command=self.__main_window.add_list_button_click_handler)
+
+        self.__my_lists = ctk.CTkButton(self, text=ml_tt, width=wh_mf, fg_color=fgc_mf, border_color=bc_mf,
+                                      height=ht_mf, text_color=tc_mf, border_width=bw_mf, hover_color=hc_mf, font=ft_mf)
+        self.__my_lists.place(relx=0.05, rely=0.55)
+        self.__my_lists.configure(command=self.__main_window.all_lists_button_click_handler)
+
+        self.__favorites = ctk.CTkButton(self, text=f_tt, width=wh_mf, fg_color=fgc_mf, border_color=bc_mf,
+                                       height=ht_mf, text_color=tc_mf, border_width=bw_mf, hover_color=hc_mf, font=ft_mf)
+        self.__favorites.place(relx=0.38, rely=0.55)
+        self.__favorites.configure(command=self.__main_window.favorites_button_click_handler)
+
+        self.__history = ctk.CTkButton(self, text=h_tt, width=wh_mf, fg_color=fgc_mf, hover_color=hc_mf,
+                                     height=ht_mf, text_color=tc_mf, border_width=bw_mf, border_color=bc_mf, font=ft_mf)
+        self.__history.place(relx=0.7, rely=0.55)
+        self.__history.configure(command=self.__main_window.history_button_click_handler)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 class MainPage(ctk.CTk):
