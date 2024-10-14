405cd0f6b9a14fe7ab7fed5869193ec193671f23
Force-in-Motion <canzdao@gmail.com>
2024-10-14 12:11:01 +0300

    feat main_page : add imports with min_page

---
+++ src/start/main_page.py
@@ -0,0 +1,10 @@
+import customtkinter as ctk
+from PIL import Image
+import sys
+
+from src.start.config_main_page import *
+from src.lists.all_lists import *
+from src.create.create_list import *
+from src.favorite.favorite_product import *
+from src.history.purchase_history import *
+from src.open.open_list import *
