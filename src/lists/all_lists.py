24ce7aef7475e090e78a091962a1f5215c0affb8
Force-in-Motion <canzdao@gmail.com>
2024-10-14 10:41:11 +0300

    feat all_lists : add class AllLists

--- src/lists/all_lists.py
+++ src/lists/all_lists.py
@@ -37,3 +37,28 @@ class MenuButtonsAllLists(ctk.CTkFrame):
         self.__config_label_add_list()
         self.__config_add_list_button()
         self.__config_menu_buttons()
+
+
+class AllLists(ctk.CTkToplevel):
+    """
+    Мэйн класс страницы, в себе формирует основной контейнер (фрейм), содержащий остальные виджеты страницы, а так же основную логику страницы
+    """
+    def __init__(self, main_window):
+        super().__init__()
+        self.__main_window = main_window
+
+        self.__load_data = sld.read_data_with_shopping_lists() if sld.check_file_shopping_lists() else {}
+        self.__load_data_purchase_history = sld.read_data_with_purchase_history() if sld.check_file_purchase_history() else {}
+
+        self.__create_shopping_list_page = None
+        self.__confirmation_request_page = None
+        self.__edit_name_shopping_list_page = None
+        self.__open_list_page = None
+
+        self.__scroll_all_lists = None
+        self.__meny_btn_all_lists = None
+
+        self.__config_window()
+        self.__config_logo()
+        self.__config_scroll_frame()
+        self.__config_menu_buttons()
