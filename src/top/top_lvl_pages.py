b271f3b62fa89bf60d2becef4464989a5cf964ad
Force-in-Motion <canzdao@gmail.com>
2024-10-14 13:19:11 +0300

    feat top_windows : add imports

---
+++ src/top/top_lvl_pages.py
@@ -0,0 +1,7 @@
+import customtkinter as ctk
+from PIL import Image
+from tkinter.messagebox import showerror
+
+from src.top.config_top_level_pages import *
+from src.load.save_and_load_data import SaveAndLoadData as sld
+from src.templates.templates import Templates
