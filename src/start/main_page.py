326bfbc19ee08c183483bdad68b36a219dbfcd1c
Force-in-Motion <canzdao@gmail.com>
2024-10-18 12:28:36 +0300

    fix description imports main_page

--- src/start/main_page.py
+++ src/start/main_page.py
@@ -1,12 +1,13 @@
+import customtkinter as ctk
+from PIL import Image
+import sys
+
 from src.start.config_main_page import *
 from src.lists.all_lists import *
 from src.create.create_list import *
 from src.favorite.favorite_product import *
 from src.history.purchase_history import *
 from src.open.open_list import *
-import customtkinter as ctk
-from PIL import Image
-import sys
 
 
 class MainFrame(ctk.CTkFrame):
